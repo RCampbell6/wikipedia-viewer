70156f134dfec7e538e8156424a460c6e135b9d4
Richard Campbell <rcampbell6@gmail.com>
2016-09-18 19:50:37 +1000

    Further development.
    Added buttons.
    Preliminary fading.

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 ﻿$(document).ready(function () {
    function getWikis(searchItem) {
-      var url = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrsearch=" + searchItem + "&gsrlimit=9&prop=pageimages|extracts&pilimit=max&pithumbsize=100&exintro=1&explaintext=1&exsentences=1&exlimit=9&callback=?";
+      var url = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrsearch=" + searchItem + "&gsrlimit=9&prop=info|pageimages|extracts&inprop=url&pilimit=max&pithumbsize=100&exintro=1&explaintext=1&exsentences=1&exlimit=9&callback=?";
       $.getJSON(url, function (wikis) {
          var keys = Object.keys(wikis.query.pages);
          var titles = [];
          var extracts = [];
+			var links = [];
+			
+			//Extract various information from each result
          keys.forEach(function (item) {
             var index = wikis.query.pages[item].index - 1;
             titles[index] = wikis.query.pages[item].title;
             extracts[index] = wikis.query.pages[item].extract;
-            console.log(titles);
-            console.log(extracts);
+				links[index] = wikis.query.pages[item].fullurl;
          });
-         var thumbs = document.getElementsByClassName("wiki");
-         for (var i = 0; i < thumbs.length; i++) {
-            thumbs[i].innerHTML = "<span>" + titles[i] + "</span><p>" + extracts[i] + "</p>";
-         }
-         console.log(wikis.query.pages[keys[0]]);
+			
+			//Find target elements and assign information
+         var thumbTitles = document.getElementsByClassName("title");
+         var thumbExtracts = document.getElementsByClassName("extract");
+         var thumbLinks = document.getElementsByClassName("chevron-button");
+			
+			$(".wiki").animate({
+				opacity: 0
+			}, 800, function () {
+				for (var i = 0; i < thumbTitles.length; i++) {
+					thumbTitles[i].innerHTML = titles[i];
+					thumbExtracts[i].innerHTML = extracts[i];
+					thumbLinks[i].href = links[i];
+				}
+				$(".wiki").animate({
+					opacity: 1
+				}, 800);
+			});         
       });
    }
 
